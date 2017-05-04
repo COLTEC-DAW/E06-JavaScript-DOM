24e6a454238f9b6e3001e76690148243f8bbe6dc
francisco fabiano neves <a2015951614@lycda.local>
2017-05-04 11:23:26 -0300

    PROGRESSO!

--- 1_tabela.js
+++ 1_tabela.js
@@ -1,17 +1,20 @@
-//Como testar?
-
-function csvToTable(csv){
-    var table = document.createElement("table");
+function csvToTable(){
+    console.log("Funcionando");
+    var csv = document.getElementsByTagName("textarea")[0].value;
+    console.log(csv);
+    var table = document.getElementsByTagName("table")[0];
     var linhas = csv.split(";");
+    console.log(linhas);
     for (var i=0; i<csv.length; i++){
         var row = document.createElement("tr");
-        var celulas = linhas.split(", ");
+        var celulas = linhas[i].split(",");
+        console.log(celulas);
         for (var j=0; j<celulas.length; j++){
             var dado = document.createElement("td");
-            td.value = celulas[j];
-            row.appendChild(td);
+            dado.value = celulas[j];
+            console.log(dado);
+            row.appendChild(dado);
         }
         table.appendChild(row);
     }
-    document.appendChild(table);
 }
