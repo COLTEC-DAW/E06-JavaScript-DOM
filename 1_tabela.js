38fc0dd567022ce744119fae4a7e01f18dbb8624
Francisco Fabiano Neves <Francisco Fabiano Neves>
2017-05-04 22:05:03 -0300

    1 Finalmente funcionando (maldito textNode, porque tão específico?)

--- 1_tabela.js
+++ 1_tabela.js
@@ -1,5 +1,4 @@
 function csvToTable(){
-    console.log("Funcionando");
     var csv = document.getElementsByTagName("textarea")[0].value;
     console.log(csv);
     var table = document.getElementsByTagName("table")[0];
@@ -7,11 +6,14 @@ function csvToTable(){
     console.log(linhas);
     for (var i=0; i<csv.length; i++){
         var row = document.createElement("tr");
+        console.log(linhas[i]);
         var celulas = linhas[i].split(",");
         console.log(celulas);
         for (var j=0; j<celulas.length; j++){
             var dado = document.createElement("td");
-            dado.value = celulas[j];
+            var txtzin = document.createTextNode(celulas[j]);
+            console.log(celulas[j]);
+            dado.appendChild(txtzin);
             console.log(dado);
             row.appendChild(dado);
         }
