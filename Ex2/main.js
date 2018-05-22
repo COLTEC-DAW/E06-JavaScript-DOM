36c4100e363d3b9ab84492ee0447f4fdae8a01ef
Ítalo Dell Areti <italo@Caxu.local>
2018-05-21 21:57:32 -0300

    Terminado

---
+++ Ex2/main.js
@@ -0,0 +1,24 @@
+function getByName(node, string) {
+  var count = 0;
+  for (var i = 0; i < node.childNodes.length; i++) {
+    var childNode = node.childNodes[i];
+    if(childNode.childNodes.length > 0)
+      count += getByName(childNode, string);
+    else if(childNode.nodeValue != null &&
+            childNode.nodeValue.indexOf(string) > -1)
+      count ++;
+  }
+  return count;
+
+}
+
+  //Teste
+        var body = document.getElementsByTagName('body')[0];
+        tag = "P";
+        console.log("A tag "+ tag + " foi encontrada " + getByName(body,tag) + " vezes.");
+
+        var tag = "SECTION";
+        console.log("A tag "+ tag + " foi encontrada " + getByName(body,tag) + " vezes.");
+
+        tag = "IMG";
+        console.log("A tag "+ tag + " foi encontrada " + getByName(body,tag) + " vezes.");
