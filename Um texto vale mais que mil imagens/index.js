46273881eeb57bed8c7109122b4237543d4c9eaf
MariSantos17 <mariana@luarinformatica.com.br>
2018-08-24 13:17:34 -0300

    ok

---
+++ Um texto vale mais que mil imagens/index.js
@@ -0,0 +1,10 @@
+function ImgTxt(){
+    var img = document.body.getElementsByTagName("img")
+    for(var i = 0; i < img.length; i++){
+      var description = img[i].alt
+      document.removeChild(img[i])
+      var p = document.createElement("p")
+      var node = document.createTextNode(description)
+      document.appendChild(node)
+    }
+  }
