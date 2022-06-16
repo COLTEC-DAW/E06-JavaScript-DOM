32bc9e112dcd4bb81a1903943167aa44658fde40
Gabriel Benvindo Begnami <gabrielbegnami1@gmail.com>
2022-06-16 13:56:00 -0300

    Lidando com Tabelas

---
+++ E06.js
@@ -0,0 +1,47 @@
+function openModal() {
+    let modal = document.querySelector(".modal");
+
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    let modal = document.querySelector(".modal");
+
+    modal.style.display = "none";
+}
+
+function createHTMLtable(titleText, csvData) {
+    let title = document.getElementById("title");
+    let tableSpace = document.getElementById("tableSpace");
+
+    let table = document.createElement("table");
+    let csvTable = csvData.split("\n");
+    let h1 = document.createElement("h1");
+    let text = document.createTextNode(titleText);
+
+    h1.appendChild(text);
+    title.appendChild(h1);
+
+    csvTable.forEach(line => {
+        let values = line.toString().split(",");
+        let tr = document.createElement("tr");
+
+        values.forEach(value => {
+            let td = document.createElement("td");
+            td.appendChild(document.createTextNode(value));
+            tr.appendChild(td);
+        })
+
+        table.appendChild(tr);
+    })
+
+    tableSpace.appendChild(table);
+}
+
+function submitFunction() {
+    let titleInputValue = document.querySelector("#titleInput").value;
+    let csvInputValue = document.querySelector("#csvInput").value;
+
+    createHTMLtable(titleInputValue, csvInputValue);
+    closeModal();
+}
