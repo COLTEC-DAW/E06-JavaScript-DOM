fb59070adbe25c2903ceeccf5ca74a19a456300b
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-06-19 18:25:29 -0300

    implementação da descrição das imagens e do GetTag

--- E06.js
+++ E06.js
@@ -11,7 +11,11 @@ function convertCSVtoHTML(){
     table_title.innerHTML = "";
     table_content.innerHTML="";
 
-    table_title.style.border = " .15rem solid black";
+    table_title.style.borderTop = " .15rem solid black";
+    table_title.style.borderRight = " .15rem solid black";
+    table_title.style.borderLeft = " .15rem solid black";
+    table_title.style.borderBottom = " .1rem solid black";
+    
     table_title.appendChild(title);
 
     let table = document.createElement("table");
@@ -38,3 +42,49 @@ function convertCSVtoHTML(){
     table_content.appendChild(table);
 
 }
+
+function img_to_description(){
+
+    let imgs = [].slice.call(document.getElementsByTagName("img"));
+
+    imgs.forEach(img => { 
+
+        let parent_of_img = img.parentElement;
+        let description = document.createTextNode(img.alt.toString());
+        parent_of_img.removeChild(img);
+        parent_of_img.appendChild(description);
+
+    });
+}
+
+function callGetByName(){
+
+    let tag = document.getElementById("inputTag").value;
+    getByName(document.body, tag);
+}
+
+function getByName(node, elem){
+
+
+    let array_elements_tag_name = [];
+
+    for(let i = 0; i < node.childNodes.length; i++){
+
+        let childNode = node.childNodes[i];
+
+        if(childNode.childNodes.length > 0){
+
+            getByName(childNode, elem);
+        }
+        if(childNode.tagName == elem){
+
+            array_elements_tag_name.push(childNode);
+
+        }
+
+    }
+
+    console.log(array_elements_tag_name);
+    return array_elements_tag_name;
+}
+
