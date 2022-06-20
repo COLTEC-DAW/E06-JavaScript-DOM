19b751a5ef8863988ba343bd42e10346197159a9
PedroDuraesS <102993693+PedroDuraesS@users.noreply.github.com>
2022-06-20 20:02:49 -0300

    .

--- script.js
+++ script.js
@@ -8,4 +8,30 @@ function ConverseCSV_HTML()
     let Tabtitle = document.getElementById("Tabtitle");
     let Tabcontent = document.getElementById("Tabcontent");
 
+    Tabtitle.appendChild(title);
+
+    let table = document.createElement("table");
+
+    let tableCSV_HTML = csv_input.split("\n");
+
+    tableCSV_HTML.forEach(linhasTr=>{
+        
+        let palavrasLinhas = linhasTr.toString().split(",");
+        let linhaX = document.createElement("tr");
+
+        palavrasLinhas.forEach(PalavrasTd=>{
+
+            let Palavra = document.createElement("td");
+            let texto = document.createTextNode(Palavra);
+            Palavra.appendChild(texto);
+            linhaX.appendChild(Palavra);
+    
+        })
+
+        table.appendChild(linha);
+
+    });
+
+    table_content.appendChild(table);
+
 }
