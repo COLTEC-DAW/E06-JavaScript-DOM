7bdb97a00d41de3edc84fec3403b01665df077f3
Gabriel Machado Violante <101432741+ViolanteGabriel@users.noreply.github.com>
2022-06-20 07:32:25 -0300

    CSV para tabela

---
+++ functions.js
@@ -0,0 +1,29 @@
+function HTMLtable() {
+
+    let CSV_text = document.getElementById("CSV_text").value.toString();
+    let tableArea = document.getElementById("tableArea");
+    let tableLines = CSV_text.split("\n");
+
+    let tableName = document.createElement("h2");
+    let table = document.createElement("table");
+
+    tableName.appendChild( document.createTextNode( document.getElementById( "tableName" ).value.toString() ) )
+    tableArea.appendChild(tableName);
+
+    tableLines.forEach( (line) => {
+        let column = line.toString().split(", ");
+        let tableLine = document.createElement("tr");
+
+        column.forEach( (element) => {
+            let tableElement = document.createElement("td");
+            tableLine.appendChild(tableElement);
+            tableElement.appendChild(document.createTextNode(element));
+        } )
+
+        table.appendChild(tableLine);
+
+    } )
+
+    tableArea.appendChild(table);
+    document.getElementById("tableInfo").style.display = "none";
+}
