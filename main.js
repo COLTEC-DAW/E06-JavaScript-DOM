0245ed231ce98a71ccda36dd731c358f44d8bbfe
Dannillouou <danielalmemiranda@gmail.com>
2022-06-21 22:18:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var body = document.body;
+
+//var csvForm = document.body.getElementsByClassName("csvFile");
+
+//var arrayDeAlgo = Array.from(document.querySelector('#csvFile').value);
+
+/*console.log(document.querySelector('#csvFile').value);
+console.log("222");*/
+
+let csv = document.getElementById('csvFile').value;
+console.log(csv);
+
+//let csvData = csv.split(",");
+let csvData = "90";
+console.log(csvData);
+
