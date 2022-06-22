8933871533aa3eace354a29a57f090fb881d501a
Daniel Miranda <85716954+Dannillouou@users.noreply.github.com>
2022-06-22 15:16:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var body = document.body;
+let body = document.body;
 
 //var csvForm = document.body.getElementsByClassName("csvFile");
 
@@ -7,10 +7,35 @@ var body = document.body;
 /*console.log(document.querySelector('#csvFile').value);
 console.log("222");*/
 
-let csv = document.getElementById('csvFile').value;
+/*let csv = document.getElementById('csvFile').value;
+console.log(csv);*/
+
+let csv = document.getElementById('csvFiletxt').value;
 console.log(csv);
 
-//let csvData = csv.split(",");
-let csvData = "90";
-console.log(csvData);
+let csvLines = csv.split(/\r?\n/);
+console.log(csvLines);
 
+//criando tabela e atributos
+let table = document.createElement('table');
+let tableAttr = document.createAttribute('border');
+let tableHead = document.createElement('thead');
+
+//adicionando cabeçalho e estilo
+tableAttr.value = 2;
+table.setAttributeNode(tableAttr);
+table.appendChild(tableHead);
+
+//adicionando tabela toda
+body.appendChild(table);
+
+//adicionando elementos na tabela
+let row1 = document.createElement('tr');
+for(i = 0, j = 0; i < csvLines[0].length; i++, j++){
+    let rowData
+    let heading = document.createElement('th');
+    heading.innerHTML = csvLines[i][j];
+}
+
+/*joao, jlkdsjalfk, klsadjflkajs
+aklsdfj, cpovizx, qwembwer*/
